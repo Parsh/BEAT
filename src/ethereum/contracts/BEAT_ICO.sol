762b0851a1bcbd44df584d5b4b6376b54697f322
parsh24 <parsh.cosmos11@gmail.com>
2018-08-28 20:09:41 +0530

    Buy tokens mechanism

--- src/ethereum/contracts/BEAT_ICO.sol
+++ src/ethereum/contracts/BEAT_ICO.sol
@@ -22,5 +22,24 @@ contract BEAT_ICO{
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
+
+    function mul(uint256 _a, uint256 _b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested. This is multiply fxn from SafeMath
+        if (_a == 0) {
+            return 0;
+        }
+        
+        uint256 c = _a * _b;
+        require(c / _a == _b);
+        
+        return c;
+    }
+        
+    function buyTokens(uint _numberOfTokens) public hasTokenSupply(_numberOfTokens) payable {
+           
+        require(msg.value == mul(_numberOfTokens, tokenPrice), "Price paid is not equal");
+        require(tokenContract.transfer(msg.sender, _numberOfTokens), "Transfer failed");
+    }
         
 }
