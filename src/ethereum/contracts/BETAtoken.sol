d9c3a418c9f7a14422dcd47470d1050719c9080e
parsh24 <parsh.cosmos11@gmail.com>
2018-08-28 17:00:46 +0530

    Events and Modifier

--- src/ethereum/contracts/BETAtoken.sol
+++ src/ethereum/contracts/BETAtoken.sol
@@ -10,6 +10,22 @@ contract BetaToken{
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
         
+    modifier sufficientBalance(uint _value){
+        require(balanceOf[msg.sender] >= _value, "Do not have enough BEATs");
+        _;
+    }
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to, 
+        uint _value);
+    
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value);
+    
+
     constructor(uint _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
