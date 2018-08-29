34d18fc55291704a19166bc730e2d09252364936
parsh24 <parsh.cosmos11@gmail.com>
2018-08-29 20:28:42 +0530

    ICO design

--- src/components/ICO.js
+++ src/components/ICO.js
@@ -3,72 +3,99 @@ import web3 from '../ethereum/production/web3';
 
 const ICO = props => {
   const jumbotron = (
-    <div className="jumbotron text-center">
-      <h1 className="display-3 mb-4 blue-text">
-        <strong>BEAT ICO</strong>
-      </h1>
-      <p className="h4-responsive">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
+    <div className="z-depth-5 animated fadeIn">
+      <div
+        className="jumbotron text-center text-white"
+        style={{
+          backgroundImage:
+            'url(https://coolbackgrounds.io/images/backgrounds/compute-83062381.png)'
+        }}
+      >
+        <h1 className="display-3 mb-4 blue-text">
+          <strong>BEAT ICO</strong>
+        </h1>
+        <p className="h4-responsive mb-5">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+      </div>
     </div>
   );
 
   const buyCard = (
-    <div className="card mt-5">
-      <div className="card-body text-center">
-        <h2 className="card-title">Buy BEATs</h2>
-        <h4 className="text-muted">You currently have: {props.tokenBalance}</h4>
-        <div className="md-form">
-          <input
-            type="text"
-            className="form-control text-center w-75 m-auto"
-            value={props.buyTokens}
-            onChange={event => {
-              props.onInputChange(event);
-            }}
-            placeholder="Number of tokens"
-          />
+    <div className="z-depth-5 mt-5 animated fadeIn">
+      <div
+        className="card"
+        style={{
+          backgroundImage:
+            'url(https://coolbackgrounds.io/images/backgrounds/compute-83062381.png)',
+          backgroundPosition: 'left'
+        }}
+      >
+        <div className="card-body text-center text-white">
+          <h2 className="card-title">Buy BEATs</h2>
+          <h4>You currently have: {props.tokenBalance}</h4>
+          <div className="md-form">
+            <input
+              type="text"
+              className="form-control text-center w-75 m-auto text-white"
+              value={props.buyTokens}
+              onChange={event => {
+                props.onInputChange(event);
+              }}
+            />
+          </div>
+          <button className="btn btn-primary" onClick={props.onBuy}>
+            Buy !
+          </button>
         </div>
-        <button className="btn btn-primary w-25" onClick={props.onBuy}>
-          Buy !
-        </button>
       </div>
     </div>
   );
 
   const infoCard = (
-    <div className="mt-5">
-      <div className="card-body text-center">
-        <h3 className="card-title">BEAT Token</h3>
-        <h4 className="text-muted mt-3">
-          Token Price: {web3.utils.fromWei(props.tokenPrice, 'ether')} ether
-        </h4>
-        <h5 className="text-muted mt-3 ">
-          Tokens Sold: {props.tokensSold}/{props.initialICOFund}
-        </h5>
-        <div className="progress md-progress mt-3" style={{ height: 10 }}>
+    <div className="z-depth-5 animated fadeIn">
+      <div
+        className="card mt-5"
+        style={{
+          backgroundImage:
+            'url(https://coolbackgrounds.io/images/backgrounds/compute-83062381.png)',
+          backgroundPosition: 'right'
+        }}
+      >
+        <div className="card-body text-center text-white">
+          <h2 className="card-title">BEAT Token</h2>
+          <h3 className="mt-3">
+            Token Price: {web3.utils.fromWei(props.tokenPrice, 'ether')} ether
+          </h3>
+          <h4 className="mt-3 ">
+            Tokens Sold: {props.tokensSold}/{props.initialICOFund}
+          </h4>
           <div
-            className="progress-bar progress-bar-striped"
-            role="progressbar"
-            style={{
-              width: `${(props.tokensSold / props.initialICOFund) * 100}%`,
-              height: '10px'
-            }}
-          />
+            className="progress md-progress mt-4 mb-4"
+            style={{ height: 10 }}
+          >
+            <div
+              className="progress-bar progress-bar-striped"
+              role="progressbar"
+              style={{
+                width: `${(props.tokensSold / props.initialICOFund) * 100}%`,
+                height: '10px'
+              }}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 
   return (
-    <div>
+    <div className="mt-5 mb-5">
       {jumbotron}
       <div className="container" style={{ marginTop: '75px' }}>
         <div className="row">
-          <div className="col-sm-4">{infoCard}</div>
-
-          <div className="col-sm-8">{buyCard}</div>
+          <div className="col-sm-5">{infoCard}</div>
+          <div className="col-sm-7">{buyCard}</div>
         </div>
       </div>
     </div>
