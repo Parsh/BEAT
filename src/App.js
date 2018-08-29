d9366e39adfefe3907918e81c918199dd34a8e38
parsh24 <parsh.cosmos11@gmail.com>
2018-08-29 13:59:38 +0530

    Connection test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import ICO from './components/ICO';
 import './App.css';
 
+import token from './ethereum/production/BEATtoken';
+import ico from './ethereum/production/BEAT_ICO';
+
 class App extends Component {
+  async componentDidMount() {
+    const supply = await token.methods.totalSupply().call();
+    const admin = await ico.methods.admin().call();
+
+    console.log('Supply: ', supply);
+    console.log('Admin: ', admin);
+  }
+
   render() {
     return <ICO />;
   }
